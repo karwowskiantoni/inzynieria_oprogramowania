a5183cadca30bfc60d4e63d8ff56d9d3a6ee2f89
w-lugowska <wercik2610@gmail.com>
2021-06-09 16:09:54 +0200

    wyświetlanie informacji o użytkowniku

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,13 +1,41 @@
 import { PlaceHolderPage } from "./PlaceHolderPage";
+import { sendRequestWithToken } from "../utils/Authorization";
 import { Redirect } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Button, Container } from "react-bootstrap";
 
 export function HomePage() {
+  const [user, setUser] = useState("[]");
+  const ownerId = localStorage.getItem("id");
+
+  useEffect(() => {
+    async function fetchAPI() {
+      let response;
+      if (localStorage.getItem("type") === "owner") {
+        response = await sendRequestWithToken(
+          `owners/${localStorage.getItem("id")}`
+        );
+      } else {
+        response = await sendRequestWithToken(`vets/${localStorage.getItem("id")}`);
+      }
+      let json = await response.json();
+      setUser(json);
+    }
+    fetchAPI();
+  }, []);
+
   return (
     <div>
       {localStorage.getItem("token") === null ? (
         <Redirect to={"/login"} />
       ) : null}
-      <PlaceHolderPage text={"Jesteś w systemie obsługi wizyt w klinice weterynaryjnej!"} />
+      <PlaceHolderPage
+        text={"Jesteś w systemie obsługi wizyt w klinice weterynaryjnej!"}
+      />
+      <Container style={{ padding: 10 }}>
+        <h2> Twoje dane </h2>
+        <h3> {user.firstName} {user.surname} </h3>
+      </Container>
     </div>
   );
 }
